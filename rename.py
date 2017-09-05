6944b43ca9772056ae8307bd5e01138490e1ce62
MarvinJWendt <github@marvin-wendt.de>
2017-09-05 23:08:14 +0200

    now removing duplicates

--- rename.py
+++ rename.py
@@ -3,22 +3,25 @@ import sys
 import hashlib
 
 def hash(file, method):
-    f = open(file, 'rb')
-    sum = ""
-    if method == "sha1":
-        sum = hashlib.sha1(f.read()).hexdigest()
-    elif method == "sha224":
-        sum = hashlib.sha224(f.read()).hexdigest()
-    elif method == "sha256":
-        sum = hashlib.sha256(f.read()).hexdigest()
-    elif method == "sha384":
-        sum = hashlib.sha384(f.read()).hexdigest()
-    elif method == "sha512":
-        sum = hashlib.sha512(f.read()).hexdigest()
-    elif method == "md5":
-        sum = hashlib.md5(f.read()).hexdigest()
-    f.close()
-    return sum
+    if not os.path.isdir(file):
+        f = open(file, 'rb')
+        sum = ""
+        if method == "sha1":
+            sum = hashlib.sha1(f.read()).hexdigest()
+        elif method == "sha224":
+            sum = hashlib.sha224(f.read()).hexdigest()
+        elif method == "sha256":
+            sum = hashlib.sha256(f.read()).hexdigest()
+        elif method == "sha384":
+            sum = hashlib.sha384(f.read()).hexdigest()
+        elif method == "sha512":
+            sum = hashlib.sha512(f.read()).hexdigest()
+        elif method == "md5":
+            sum = hashlib.md5(f.read()).hexdigest()
+        f.close()
+        return sum
+    else:
+        return "dir"
 
 for file in os.listdir('.'):
     if not file == "rename.py":
@@ -29,6 +32,21 @@ for file in os.listdir('.'):
             sum = hash(file, sys.argv[1])
         else:
             sum = hash(file, "md5")
+        try:
+            print("Trying to rename: " + file)
 
-        print(file + ' --> ' + sum)
-        os.rename(file, sum + file_extension)
+            if os.path.isfile(sum + file_extension):
+                print("file " + sum + file_extension + " already exists")
+                if not file == (sum + file_extension):
+                    print("Removing: " + file + " because it is a duplicate")
+                    os.remove(file)
+
+            elif not sum == "dir":
+                os.rename(file, sum + file_extension)
+                print(file + ' --> ' + sum + file_extension)
+            else:
+                print("Skipping directory " + file)
+        except Exception as e:
+            print("Error displaying file name.")
+
+        print("")
